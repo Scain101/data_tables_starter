3d134fa6031217feb086b98c592ed12cfd09e53d
Scott Cain <scain101@gmail.com>
2022-06-21 10:01:15 -0600

    Imported_Projects_06-19-22

--- src/index.js
+++ src/index.js
@@ -4,11 +4,29 @@ let table;
 
 window.loadTable = (json) => {
   const obj = JSON.parse(json);
-  // const data = obj.data;
-  const columns = [];
-  const data = [];
+  const data = obj.data;
+  const columnType = obj.columnType;
+
+  const customerColumns = [
+    { title: "Company:", data: "fieldData.CompanyName" },
+    { title: "City", data: "fieldData.City" },
+    { title: "State", data: "fieldData.State" },
+    { title: "Zip", data: "fieldData..Zip" },
+  ];
+  const admissionsColumns = [
+    { title: "Category", data: "fieldData.Category" },
+    { title: "City", data: "fieldData.Program" },
+    { title: "State", data: "fieldData.Grade" },
+  ];
+
+  const columns =
+    columnType === "Customers" ? customerColumns : admissionsColumns;
   table = $("#dtable").DataTable({
     columns,
     data,
+    pageLength: 60,
+    scrollY: "800px",
   });
 };
+
+//add a row click to the table rows
